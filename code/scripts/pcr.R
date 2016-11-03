957ac890141392e1011871824fd6c46d26b9ad25
Alexander Lee <tofu.da.beast.42@gmail.com>
2016-11-02 22:32:58 -0700

    Created pcr.R, added the functionality. Should approximately fulfill the conditions given in instructions.

---
+++ code/scripts/pcr.R
@@ -0,0 +1,30 @@
+library(pls)
+credit_train <- read.csv('../../data/train-set-data.csv')
+credit_test <- read.csv('../../data/test-set-data.csv')
+
+#run the pcr on the training data
+set.seed(42)
+pcr_mod <- pcr(Balance ~ ., data = credit_train, alpha=0, 
+               scale=FALSE, validation="CV")
+
+#find best model
+best_pcr_number <- which.min(pcr_mod$validation$PRESS)
+best_pcr <- pcr_mod$coefficients[4:14,1,best_pcr_number]
+
+#calculate the MSE using the test data
+pcr_pred <- predict(pcr_mod, credit_test, ncomp = best_pcr)
+pcr_mse <- mean((pcr_pred - credit_test[,"Balance"]) ^ 2)
+
+#plot the pcr
+validationplot(pcr_mod, val.type = "MSEP")
+
+#run the pcr on the full data
+credit <- read.csv('../../data/Credit.csv')
+credit_x <- model.matrix(Balance~., credit)[,-1]
+credit_y <- credit$Balance
+pcr_final <- pcr(credit_y ~ credit_x, scale = FALSE, ncomp = best_pcr)
+
+pcr_coefficients <- pcr_final$coefficients[3:13,1,13]
+
+rdata_output <- '../../data/best-pcr.RData'
+save(best_pcr, pcr_coefficients, pcr_mse)
