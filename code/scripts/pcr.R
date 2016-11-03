4fccb4f64e291cb5fc0d91bd158e229f4ed43d2f
Alexander Lee <tofu.da.beast.42@gmail.com>
2016-11-03 09:51:16 -0700

    pcr.R updated to not include the X columns, though it hardly mattered ultimately. best-pcr.RData created to include the relevant data.

--- code/scripts/pcr.R
+++ code/scripts/pcr.R
@@ -2,6 +2,8 @@ library(pls)
 credit_train <- read.csv('../../data/train-set-data.csv')
 credit_test <- read.csv('../../data/test-set-data.csv')
 
+credit_train <- credit_train[,-(1:3)]
+credit_test <- credit_test[,-(1:3)]
 #run the pcr on the training data
 set.seed(42)
 pcr_mod <- pcr(Balance ~ ., data = credit_train, alpha=0, 
@@ -9,10 +11,10 @@ pcr_mod <- pcr(Balance ~ ., data = credit_train, alpha=0,
 
 #find best model
 best_pcr_number <- which.min(pcr_mod$validation$PRESS)
-best_pcr <- pcr_mod$coefficients[4:14,1,best_pcr_number]
+best_pcr <- pcr_mod$coefficients[,1,best_pcr_number]
 
 #calculate the MSE using the test data
-pcr_pred <- predict(pcr_mod, credit_test, ncomp = best_pcr)
+pcr_pred <- predict(pcr_mod, credit_test, ncomp = best_pcr_number)
 pcr_mse <- mean((pcr_pred - credit_test[,"Balance"]) ^ 2)
 
 #plot the pcr
@@ -20,11 +22,12 @@ validationplot(pcr_mod, val.type = "MSEP")
 
 #run the pcr on the full data
 credit <- read.csv('../../data/Credit.csv')
-credit_x <- model.matrix(Balance~., credit)[,-1]
+credit_x <- model.matrix(Balance~., credit)[,-(1:3)]
 credit_y <- credit$Balance
-pcr_final <- pcr(credit_y ~ credit_x, scale = FALSE, ncomp = best_pcr)
+pcr_final <- pcr(credit_y ~ credit_x, scale = FALSE, 
+                 ncomp = best_pcr_number)
 
-pcr_coefficients <- pcr_final$coefficients[3:13,1,13]
+pcr_coefficients <- pcr_final$coefficients[,1,best_pcr_number]
 
 rdata_output <- '../../data/best-pcr.RData'
-save(best_pcr, pcr_coefficients, pcr_mse)
+save(best_pcr, pcr_coefficients, pcr_mse, file = rdata_output)
