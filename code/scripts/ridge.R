6ee0adc5addd720706502a4af9ce85fe7d6393b5
Alexander Lee <tofu.da.beast.42@gmail.com>
2016-11-02 21:31:57 -0700

    Added comments and removed unnecessary code.

--- code/scripts/ridge.R
+++ code/scripts/ridge.R
@@ -1,4 +1,5 @@
 library(glmnet)
+#import the train and test sets
 credit_train <- read.csv('../../data/train-set-data.csv')
 credit_test <- read.csv('../../data/test-set-data.csv')
 
@@ -9,15 +10,20 @@ train_y <- credit_train$Balance
 test_x <- model.matrix(Balance~., credit_test)[,-1]
 test_y <- credit_test$Balance
 
+#set seed to the best seed to ensure reproducibility
 set.seed(42)
+
+#perform ridge regression on training data
 ridge_mod <- cv.glmnet(train_x,
                        train_y, alpha=0, lambda=grid,
                        intercept=FALSE, standardize=FALSE)
 
 rdata_output <- '../../data/best-ridge.RData'
-best_ridge_number <- which(ridge_mod$lambda == ridge_mod$lambda.min)
-best_ridge <- ridge_mod$glmnet.fit$beta[,best_ridge_number]
 
+#find the best lambda
+best_ridge_number <- which(ridge_mod$lambda == ridge_mod$lambda.min)
+#coefficients of best model
+best_ridge <- ridge_mod$glmnet.fit$beta[,best_ridge_number]
 
 #plot the cross-validation errors in terms of the tuning parameter
 plot(ridge_mod)
@@ -32,14 +38,10 @@ credit <- read.csv('../../data/Credit.csv')
 
 credit_x <- model.matrix(Balance~., credit)[,-1]
 credit_y <- credit$Balance
-cv.glmnet(credit_x, credit_y, alpha=0, lambda=best_ridge_number,
-          intercept=FALSE, standardize=FALSE)
-
-predict(ridge_mod, s=best_ridge_number, 
-        newx = credit_x)
 
 credit_full <- glmnet(credit_x, credit_y, alpha=0)
 credit_pred <- predict(credit_full, s = best_ridge_number, type="coefficients")
 
+#save the relevant data to best-ridge.RData
 save(best_ridge, credit_pred, best_ridge_number, ridge_mse, 
      file = rdata_output)
